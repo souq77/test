dc0f6c6f86dff4cf34e447a2766f52ce4422e92d
Rabie ATTA <ratta@a101-07.iut.univ-paris8.fr>
2019-04-10 16:11:52 +0200

    PREMIER COMMIT DEV

--- ok/offreStage.java
+++ ok/offreStage.java
@@ -1,5 +1,12 @@
 package ok;
 
 public class offreStage {
+	private int id;
+	private String descriptif;
+	
+	public offreStage(int interro,String descriptif) {
+		this.id=interro;
+		this.descriptif=descriptif;
+	}
 
 }
